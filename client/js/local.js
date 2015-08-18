8494df925f9c4617382c6f2a16b095842ba6a052
mzbac <li.anchen.au@gmail.com>
2015-08-18 01:36:33 +0000

    first commit

---
+++ client/js/local.js
@@ -0,0 +1,27 @@
+var renderer = new PIXI.WebGLRenderer($(document.body).width(), $(document.body).height());
+
+// The renderer will create a canvas element for you that you can then insert into the DOM.
+document.body.appendChild(renderer.view);
+
+// You need to create a root container that will hold the scene you want to draw.
+var stage = new PIXI.Container();
+
+// This creates a texture from a 'bunny.png' image.
+var map =new Map($(document.body).width(),$(document.body).height(),$(document.body).width(),$(document.body).height(),'/img/floorMap.jpg');
+
+// Add the bunny to the scene we are building.
+stage.addChild(map);
+
+// kick off the animation loop (defined below)
+animate();
+
+function animate() {
+  // start the timer for the next animation loop
+  requestAnimationFrame(animate);
+
+  // each frame we spin the bunny around a bit
+
+
+  // this is the main render call that makes pixi draw your container and its children.
+  renderer.render(stage);
+}
---
+++ client/js/mapClass.js
@@ -0,0 +1,57 @@
+function Map(width, height,cavasWidth,canvasHeight, imageUrl) {
+    var bunnyTexture = PIXI.Texture.fromImage(imageUrl);
+    PIXI.Sprite.call(this, bunnyTexture);
+    this.position.x = 0;
+    this.position.y = 0;
+    this.width = width;
+    this.height = height;
+    this.canvasHeight=canvasHeight;
+    this.canvasWidth=cavasWidth;
+    this.interactive=true;
+    if (addWheelListener != undefined) {
+        addWheelListener(document.body, function(e) {
+            this.originalScale = this.originalScale ? this.originalScale : this.scale.x;
+            this.zoom(e.clientX, e.clientY, e.deltaY < 0);
+        }.bind(this));
+    }
+    this.mousePressPoint = [0, 0];
+}
+
+Map.prototype = Object.create(PIXI.Sprite.prototype);
+
+Map.prototype.constructor = Map;
+
+Map.prototype.mousedown = Map.prototype.touchstart = function(data) {
+
+    this.dragging = true;
+    this.mousePressPoint[0] = data.data.getLocalPosition(this.parent).x - this.position.x;
+    this.mousePressPoint[1] = data.data.getLocalPosition(this.parent).y - this.position.y;
+};
+Map.prototype.mouseup = Map.prototype.mouseupoutside =
+    this.touchend = this.touchendoutside = function(data) {
+        this.dragging = false;
+    };
+Map.prototype.mousemove = Map.prototype.touchmove = function(data) {
+    if (this.dragging) {
+        var position = data.data.getLocalPosition(this.parent);
+        if (position.x - this.mousePressPoint[0] + (this.width - this.canvasWidth) > 0 && (position.x - this.mousePressPoint[0] < 0))
+            this.position.x = position.x - this.mousePressPoint[0];
+        if (position.y - this.mousePressPoint[1] + (this.height - this.canvasHeight) > 0 && (position.y - this.mousePressPoint[1] < 0))
+            this.position.y = position.y - this.mousePressPoint[1];
+    }
+    else {
+
+    }
+};
+
+Map.prototype.zoom=function(x, y, isZoomIn) {
+ var direction = isZoomIn ? 1 : -1;
+  var factor = this.scale.x * (1 + direction * 0.1);
+  var previousScale = this.scale.x;
+  if (factor >= this.originalScale)
+    this.scale.x = this.scale.y = factor;
+  if ((this.position.x + this.width) < this.canvasWidth)
+    this.scale.x = this.scale.y = previousScale;
+  if ((this.position.y + this.height) < this.canvasHeight)
+    this.scale.x = this.scale.y = previousScale;
+}
